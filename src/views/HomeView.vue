44c56ac2d863e051114b9800d77f34b1936a6fde
Clément Brunie <clment.brunie@lyra-network.com>
2022-04-29 14:03:32 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="okb-home-section home wf-section">
+    <div class="okb-titlesection">
+      <div class="okb-titlecontainer">
+        <div class="div-block"><img src="/img/Bear-Group.svg" loading="lazy" alt="" class="image"><img
+            src="/img/TitleSection-Bears.svg" loading="lazy" alt="" class="image-2"></div>
+        <div class="div-block-2"><img src="/img/OKB-O.svg" loading="lazy" alt="" class="okb-title-o-hover"><img
+            src="/img/OKB-K.svg" loading="lazy" alt="" class="okb-title-k-hover"><img src="/img/OKB-A.svg"
+            loading="lazy" alt="" class="okb-title-a-hover"><img src="/img/OKB-Y.svg" loading="lazy" alt=""
+            class="okb-title-y-hover"></div>
+        <div class="colour-okay">
+          <div class="o-div"><img src="/img/Green-O.png" loading="lazy" alt="" class="image-10"></div>
+          <div class="k-div"><img src="/img/Blue-K.png" loading="lazy" alt="" class="image-11"></div>
+          <div class="a-div"><img src="/img/Purple-A.png" loading="lazy" alt="" class="image-8"></div>
+          <div class="y-div"><img src="/img/Yellow_Y.png" loading="lazy" alt="" class="image-9"><img
+              src="/img/TitleSection-Bears.svg" loading="lazy" alt="" class="image-2"></div>
+        </div>
+        <div class="colour-okay-tablet">
+          <div class="div-block-4">
+            <div class="o-div"><img src="/img/Green-O.png" loading="lazy" alt=""></div>
+            <div class="k-div"><img src="/img/Blue-K.png" loading="lazy" alt=""></div>
+            <div class="a-div"><img src="/img/Purple-A.png" loading="lazy" alt="" class="image-8"></div>
+            <div class="o-div-copy"><img src="/img/Yellow_Y.png" loading="lazy" alt=""></div><img
+              src="/img/TitleSection-Bears.svg" loading="lazy" alt="" class="image-2">
+          </div>
+        </div>
+        <div class="okb-hover-titles">
+          <div class="annie-are-you" style="display: none;">Annie are you</div>
+          <div class="everything-s-going-to-be" style="display: none;">Everything’s going to be</div>
+          <div class="weird-flex-but" style="display: none;">Weird flex but</div>
+          <div class="bear-markets-are" style="display: none;">Bear markets are</div>
+          <div class="emptytextblock">This is some text inside of a div block.</div>
+        </div>
+        <div class="black-okay">
+          <div class="o-div"><img src="/img/OKB-O.svg" loading="lazy"
+              data-w-id="28111576-a015-7743-53a5-4b6d09f7c00d" alt="" class="okb-title-o-hover-copy">
+          </div>
+          <div class="k-div"><img src="/img/OKB-K.svg" loading="lazy"
+              data-w-id="28111576-a015-7743-53a5-4b6d09f7c00f" alt="" class="okb-title-k-hover-copy">
+          </div>
+          <div class="a-div"><img src="/img/OKB-A.svg" loading="lazy"
+              data-w-id="28111576-a015-7743-53a5-4b6d09f7c011" alt="" class="okb-title-a-hover-copy">
+          </div>
+          <div class="y-div">
+            <div class="div-block-5"><img src="/img/OKB-Y.svg" loading="lazy"
+                data-w-id="28111576-a015-7743-53a5-4b6d09f7c014" alt="" class="okb-title-y-hover-copy">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
